eb1b9b1da0faa4b14f9303a31dbfb218f16946d8
MauraKelleher25 <69922591+MauraKelleher25@users.noreply.github.com>
2024-05-06 11:08:27 -0400

    Update tutorial_dataset.py - changing the coloring of images (gray to RGB)

--- tutorial_dataset.py
+++ tutorial_dataset.py
@@ -34,8 +34,8 @@ class MyDataset(Dataset):
         target = cv2.resize(target, self.target_size, interpolation=cv2.INTER_AREA)
 
         # Do not forget that OpenCV read images in BGR order.
-        source = cv2.cvtColor(source, cv2.COLOR_BGR2RGB)
-        target = cv2.cvtColor(target, cv2.COLOR_BGR2RGB)
+        source = cv2.cvtColor(source, cv2.COLOR_GRAY2RGB)
+        target = cv2.cvtColor(target, cv2.COLOR_GRAY2RGB)
 
         # Normalize source images to [0, 1].
         source = source.astype(np.float32) / 255.0
